e7e9dffbf18a3a7f38d2898c3e6de3fb95a1ad87
sikso1892 <sikso1892@gmail.com>
2023-09-06 14:24:30 +0900

    1-4 JSX

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
-    <div>
-      <Hello/>
-      <Hello/>
-      <Hello/>
-    </div>
+    <>
+      {/* 주석 */}
+      /* 주석 X */
+      <Hello
+        // 주석
+      />
+      <div style={style}>{name}</div>
+      <div className='gray-box'></div>
+    </>
   );
 }
 
