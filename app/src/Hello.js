a15a1ab2f6835edcaa2f5caffc62ca7a2315f21a
sikso1892 <sikso1892@gmail.com>
2023-09-11 10:39:22 +0900

    기본사용법

--- app/src/Hello.js
+++ app/src/Hello.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Hello({name, color}){
-    return <div style={{color}}>안녕하세요. {name}</div>
+function Hello({name, color, isSpecial}){
+    return (
+        <div style={{color}}>
+            { isSpecial ? <b>*</b> : null}
+            안녕하세요. {name}
+        </div>
+    )
 }
 
 Hello.defaultProps = {
