bb63ec6eeb6685c7cf438422a9fdb13ebafe53e1
sikso1892 <sikso1892@gmail.com>
2023-09-11 11:00:42 +0900

    1-7 useState를 통해 컴포넌트에서 바뀌는 값 관리하기

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 import './App.css';
+
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name='react' color='red' isSpecial/>
-      <Hello color='pink'/>
-    </Wrapper>
+    <Counter/>
   );
 }
 
