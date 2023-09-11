6108f2871d3ea195c5508cfa7a281076c112f095
sikso1892 <sikso1892@gmail.com>
2023-09-11 10:26:12 +0900

    defaultProps로 기본값 설정

--- app/src/App.js
+++ app/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <Hello name='react' color='red'/>
+      <Hello color='pink'/>
     </>
   );
 }
--- app/src/Hello.js
+++ app/src/Hello.js
@@ -4,4 +4,8 @@ function Hello({name, color}){
     return <div style={{color}}>안녕하세요. {name}</div>
 }
 
+Hello.defaultProps = {
+    name: '이름없음'
+}
+
 export default Hello;
