ac95bd7229e0568b11deb04af4d8a29bac26f1d8
sikso1892 <sikso1892@gmail.com>
2023-09-12 10:32:11 +0900

    1-11. 배열 랜더링하기

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import InputSample from './inputSample';
+import UserList from './UserList';
 
 function App() {
   return (
-    <InputSample/>
+    <UserList />
   );
 }
 
